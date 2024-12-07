386353fa908931e7d5e23b9b887523bb29e4220f
newdna <al3xandersteven@gmail.com>
2024-12-07 23:06:39 +0700

    fix: removed access token from sample command

--- sample_command.sh
+++ sample_command.sh
@@ -1,1 +1,1 @@
-python uploader.py "http://localhost:8000/submitChallenge" "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" "./test.zip" --title "Example Challenge 2" --link "https://github.com/example/repo" --points 100 --total_test_case 5 --tags javascript backend API
+python uploader.py "http://localhost:8000/submitChallenge" \<your GitHub Personal Access Token\> "./test.zip" --title "Example Challenge 2" --link "https://github.com/example/repo" --points 100 --total_test_case 5 --tags javascript backend API
